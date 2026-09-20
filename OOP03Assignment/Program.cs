f6c454eb6930fc0b93764152e7cf2292d4de6f6c
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2026-09-20 07:27:58 +0300

    Part01_Question01

--- OOP03Assignment/Program.cs
+++ OOP03Assignment/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
+
+            // Part 1
+            #region Question01
+            // Question A:
+            // Overloading Allows a class to have multiple methods with the same name but different parameters (different in type, order, or number)
+            // Overriding Allows a derived class to provide a specific implementation of a method that is already defined in its base class
             
+            // Question B:
+            // Static Binding: resolved at compile time, faster, reference type decides which method implementation to call,
+            // happens with method overloading, method hiding, non-virtual methods, static methods
+            // Dynamic Binding: resolved at runtime, slower, object decides which method implementation to call,
+            // happens with: method overriding, method hiding, and interface methods
+
+            #endregion
         }
     }
 }
